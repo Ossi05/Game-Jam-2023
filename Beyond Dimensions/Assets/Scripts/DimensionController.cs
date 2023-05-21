413574890ec1360517aeae7790e20b18084de98a
Ossi <ossijuhani.ikonen@gmail.com>
2023-05-21 11:19:24 +0300

    animations

--- Beyond Dimensions/Assets/Scripts/DimensionController.cs
+++ Beyond Dimensions/Assets/Scripts/DimensionController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class DimensionController : MonoBehaviour {
     [SerializeField] GameObject whiteObjects;
     [SerializeField] GameObject blackObjects;
-    [SerializeField] Animator playerAnimator;
+    [SerializeField] SpriteRenderer bullet;
+    [SerializeField] SpriteRenderer player;
     bool blackDimension = false;
     void Awake()
     {
+        bullet.color = Color.white;
         blackObjects.SetActive(false);
         whiteObjects.SetActive(true);
     }
@@ -18,19 +20,20 @@ public class DimensionController : MonoBehaviour {
         blackDimension = !blackDimension;
         if (blackDimension)
         {
-            playerAnimator.SetLayerWeight(1, 1f);
-            playerAnimator.SetLayerWeight(0, 0f);
-
+            
+            bullet.color = Color.black;
+            player.color = Color.black;
             blackObjects.SetActive(true);
             whiteObjects.SetActive(false);
             Camera.main.backgroundColor = Color.white;
         }
         else
         {
-            playerAnimator.SetLayerWeight(1, 0f);
-            playerAnimator.SetLayerWeight(0, 1f);
+            
             whiteObjects.SetActive(true);
             blackObjects.SetActive(false);
+            bullet.color = Color.white;
+            player.color = Color.white;
             Camera.main.backgroundColor = Color.black;
         }
     }
